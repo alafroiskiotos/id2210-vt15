cf7e11caa74785ee1b348ce07b2006774a12d28b
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-05-01 12:42:19 +0200

    Added compare method to sort

--- src/main/java/se/kth/swim/ViewNode.java
+++ src/main/java/se/kth/swim/ViewNode.java
@@ -2,7 +2,7 @@ package se.kth.swim;
 
 import se.sics.p2ptoolbox.util.network.NatedAddress;
 
-public class ViewNode {
+public class ViewNode implements Comparable<ViewNode>{
 	
 	private final NatedAddress peer;
 	private Integer infectedTimes;
@@ -38,5 +38,14 @@ public class ViewNode {
 	@Override
 	public int hashCode() {
 		return peer.hashCode();
+	}
+
+	public int compareTo(ViewNode other) {
+		if (this.infectedTimes > other.getInfectedTimes())
+			return 1;
+		else if (this.infectedTimes.equals(other.getInfectedTimes()))
+			return 0;
+		else
+			return -1;
 	}
 }
