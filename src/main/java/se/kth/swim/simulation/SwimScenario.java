9df16dda910eefe30321478d7a2fdd7a24e5a751
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-05-21 17:24:49 +0200

    Kill some nodes

--- src/main/java/se/kth/swim/simulation/SwimScenario.java
+++ src/main/java/se/kth/swim/simulation/SwimScenario.java
@@ -53,238 +53,264 @@ import se.sics.p2ptoolbox.util.network.impl.BasicNatedAddress;
  */
 public class SwimScenario {
 
-    private static long seed;
-    private static InetAddress localHost;
+	private static long seed;
+	private static InetAddress localHost;
 
-    private static CroupierConfig croupierConfig = new CroupierConfig(10, 5, 2000, 1000); 
-    static {
-        try {
-            localHost = InetAddress.getByName("127.0.0.1");
-        } catch (UnknownHostException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+	private static CroupierConfig croupierConfig = new CroupierConfig(10, 5,
+			2000, 1000);
+	static {
+		try {
+			localHost = InetAddress.getByName("127.0.0.1");
+		} catch (UnknownHostException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    //Make sure that your dead link set reflect the nodes in your system
-    private static final Map<Integer, Set<Pair<Integer, Integer>>> deadLinksSets = new HashMap<Integer, Set<Pair<Integer, Integer>>>();
+	// Make sure that your dead link set reflect the nodes in your system
+	private static final Map<Integer, Set<Pair<Integer, Integer>>> deadLinksSets = new HashMap<Integer, Set<Pair<Integer, Integer>>>();
 
-    static {
-        Set<Pair<Integer, Integer>> deadLinks;
+	static {
+		Set<Pair<Integer, Integer>> deadLinks;
 
-        deadLinks = new HashSet<Pair<Integer, Integer>>();
-        deadLinks.add(Pair.with(10, 12));
-        deadLinks.add(Pair.with(12, 10));
-        deadLinksSets.put(1, deadLinks);
+		deadLinks = new HashSet<Pair<Integer, Integer>>();
+		deadLinks.add(Pair.with(10, 12));
+		deadLinks.add(Pair.with(12, 10));
+		deadLinksSets.put(1, deadLinks);
 
-        deadLinks = new HashSet<Pair<Integer, Integer>>();
-        deadLinks.add(Pair.with(10, 12));
-        deadLinks.add(Pair.with(12, 10));
-        deadLinks.add(Pair.with(13, 10));
-        deadLinksSets.put(2, deadLinks);
-    }
+		deadLinks = new HashSet<Pair<Integer, Integer>>();
+		deadLinks.add(Pair.with(10, 12));
+		deadLinks.add(Pair.with(12, 10));
+		deadLinks.add(Pair.with(13, 10));
+		deadLinksSets.put(2, deadLinks);
+	}
 
-    //Make sure disconnected nodes reflect your nodes in the system
-    private static final Map<Integer, Set<Integer>> disconnectedNodesSets = new HashMap<Integer, Set<Integer>>();
+	// Make sure disconnected nodes reflect your nodes in the system
+	private static final Map<Integer, Set<Integer>> disconnectedNodesSets = new HashMap<Integer, Set<Integer>>();
 
-    static {
-        Set<Integer> disconnectedNodes;
+	static {
+		Set<Integer> disconnectedNodes;
 
-        disconnectedNodes = new HashSet<Integer>();
-        disconnectedNodes.add(10);
-        disconnectedNodesSets.put(1, disconnectedNodes);
+		disconnectedNodes = new HashSet<Integer>();
+		disconnectedNodes.add(10);
+		disconnectedNodesSets.put(1, disconnectedNodes);
 
-        disconnectedNodes = new HashSet<Integer>();
-        disconnectedNodes.add(10);
-        disconnectedNodes.add(12);
-        disconnectedNodesSets.put(2, disconnectedNodes);
-    }
+		disconnectedNodes = new HashSet<Integer>();
+		disconnectedNodes.add(10);
+		disconnectedNodes.add(12);
+		disconnectedNodesSets.put(2, disconnectedNodes);
+	}
 
-    static Operation1<StartAggregatorCmd, Integer> startAggregatorOp = new Operation1<StartAggregatorCmd, Integer>() {
+	static Operation1<StartAggregatorCmd, Integer> startAggregatorOp = new Operation1<StartAggregatorCmd, Integer>() {
 
-        public StartAggregatorCmd generate(final Integer nodeId) {
-            return new StartAggregatorCmd<AggregatorComp, NatedAddress>() {
-                private NatedAddress aggregatorAddress;
+		public StartAggregatorCmd generate(final Integer nodeId) {
+			return new StartAggregatorCmd<AggregatorComp, NatedAddress>() {
+				private NatedAddress aggregatorAddress;
 
-                public Class getNodeComponentDefinition() {
-                    return AggregatorComp.class;
-                }
+				public Class getNodeComponentDefinition() {
+					return AggregatorComp.class;
+				}
 
-                public AggregatorComp.AggregatorInit getNodeComponentInit() {
-                    aggregatorAddress = new BasicNatedAddress(new BasicAddress(localHost, 23456, nodeId));
-                    return new AggregatorComp.AggregatorInit(aggregatorAddress);
-                }
+				public AggregatorComp.AggregatorInit getNodeComponentInit() {
+					aggregatorAddress = new BasicNatedAddress(new BasicAddress(
+							localHost, 23456, nodeId));
+					return new AggregatorComp.AggregatorInit(aggregatorAddress);
+				}
 
-                public NatedAddress getAddress() {
-                    return aggregatorAddress;
-                }
+				public NatedAddress getAddress() {
+					return aggregatorAddress;
+				}
 
-            };
-        }
-    };
+			};
+		}
+	};
 
-    static Operation1<StartNodeCmd, Integer> startNodeOp = new Operation1<StartNodeCmd, Integer>() {
+	static Operation1<StartNodeCmd, Integer> startNodeOp = new Operation1<StartNodeCmd, Integer>() {
 
-        public StartNodeCmd generate(final Integer nodeId) {
-            return new StartNodeCmd<HostComp, NatedAddress>() {
-                private NatedAddress nodeAddress;
+		public StartNodeCmd generate(final Integer nodeId) {
+			return new StartNodeCmd<HostComp, NatedAddress>() {
+				private NatedAddress nodeAddress;
 
-                public Class getNodeComponentDefinition() {
-                    return HostComp.class;
-                }
+				public Class getNodeComponentDefinition() {
+					return HostComp.class;
+				}
 
-                public HostComp.HostInit getNodeComponentInit(NatedAddress aggregatorServer, Set<NatedAddress> bootstrapNodes) {
-                    if (nodeId % 2 == 0) {
-                        //open address
-                        nodeAddress = new BasicNatedAddress(new BasicAddress(localHost, 12345, nodeId));
-                    } else {
-                        //nated address
-                        nodeAddress = new BasicNatedAddress(new BasicAddress(localHost, 12345, nodeId), NatType.NAT, bootstrapNodes);
-                    }
-                    /**
-                     * we don't want all nodes to start their pseudo random
-                     * generators with same seed else they might behave the same
-                     */
-                    long nodeSeed = seed + nodeId;
-                    return new HostComp.HostInit(nodeAddress, bootstrapNodes, aggregatorServer, nodeSeed, croupierConfig);
-                }
+				public HostComp.HostInit getNodeComponentInit(
+						NatedAddress aggregatorServer,
+						Set<NatedAddress> bootstrapNodes) {
+					if (nodeId % 2 == 0) {
+						// open address
+						nodeAddress = new BasicNatedAddress(new BasicAddress(
+								localHost, 12345, nodeId));
+					} else {
+						// nated address
+						nodeAddress = new BasicNatedAddress(new BasicAddress(
+								localHost, 12345, nodeId), NatType.NAT,
+								bootstrapNodes);
+					}
+					/**
+					 * we don't want all nodes to start their pseudo random
+					 * generators with same seed else they might behave the same
+					 */
+					long nodeSeed = seed + nodeId;
+					return new HostComp.HostInit(nodeAddress, bootstrapNodes,
+							aggregatorServer, nodeSeed, croupierConfig);
+				}
 
-                public Integer getNodeId() {
-                    return nodeId;
-                }
+				public Integer getNodeId() {
+					return nodeId;
+				}
 
-                public NatedAddress getAddress() {
-                    return nodeAddress;
-                }
+				public NatedAddress getAddress() {
+					return nodeAddress;
+				}
 
-                public int bootstrapSize() {
-                    return 5;
-                }
+				public int bootstrapSize() {
+					return 5;
+				}
 
-            };
-        }
-    };
+			};
+		}
+	};
 
-    static Operation1<KillNodeCmd, Integer> killNodeOp = new Operation1<KillNodeCmd, Integer>() {
+	static Operation1<KillNodeCmd, Integer> killNodeOp = new Operation1<KillNodeCmd, Integer>() {
 
-        public KillNodeCmd generate(final Integer nodeId) {
-            return new KillNodeCmd() {
-                public Integer getNodeId() {
-                    return nodeId;
-                }
-            };
-        }
+		public KillNodeCmd generate(final Integer nodeId) {
+			return new KillNodeCmd() {
+				public Integer getNodeId() {
+					return nodeId;
+				}
+			};
+		}
 
-    };
+	};
 
-    //Usable NetworkModels:
-    //1. UniformRandomModel
-    //parameters: minimum link latency, maximum link latency
-    //by default Simulator starts with UniformRandomModel(50, 500), so minimum link delay:50ms, maximum link delay:500ms
-    //2. DeadLinkNetworkModel
-    //composite network model that can be built on any other network model
-    //parameters: network model, set of dead links (directed links)
-    //Pair<1,2> means if node 1 will try to send a message to node 2, the simulator will drop this message, since this is a dead link
-    //3. DisconnectedNodesNetworkModel
-    //composite network model that can be built on any other network model
-    //parameters: network model, set of disconnected nodes
-    //a disconnected node will not be able to send or receive messages
-    static Operation1<ChangeNetworkModelCmd, Integer> disconnectedNodesNMOp = new Operation1<ChangeNetworkModelCmd, Integer>() {
+	// Usable NetworkModels:
+	// 1. UniformRandomModel
+	// parameters: minimum link latency, maximum link latency
+	// by default Simulator starts with UniformRandomModel(50, 500), so minimum
+	// link delay:50ms, maximum link delay:500ms
+	// 2. DeadLinkNetworkModel
+	// composite network model that can be built on any other network model
+	// parameters: network model, set of dead links (directed links)
+	// Pair<1,2> means if node 1 will try to send a message to node 2, the
+	// simulator will drop this message, since this is a dead link
+	// 3. DisconnectedNodesNetworkModel
+	// composite network model that can be built on any other network model
+	// parameters: network model, set of disconnected nodes
+	// a disconnected node will not be able to send or receive messages
+	static Operation1<ChangeNetworkModelCmd, Integer> disconnectedNodesNMOp = new Operation1<ChangeNetworkModelCmd, Integer>() {
 
-        public ChangeNetworkModelCmd generate(Integer setIndex) {
-            NetworkModel baseNetworkModel = new UniformRandomModel(50, 500);
-            NetworkModel compositeNetworkModel = new DisconnectedNodesNetworkModel(setIndex, baseNetworkModel, disconnectedNodesSets.get(setIndex));
-            return new ChangeNetworkModelCmd(compositeNetworkModel);
-        }
-    };
+		public ChangeNetworkModelCmd generate(Integer setIndex) {
+			NetworkModel baseNetworkModel = new UniformRandomModel(50, 500);
+			NetworkModel compositeNetworkModel = new DisconnectedNodesNetworkModel(
+					setIndex, baseNetworkModel,
+					disconnectedNodesSets.get(setIndex));
+			return new ChangeNetworkModelCmd(compositeNetworkModel);
+		}
+	};
 
-    static Operation1<ChangeNetworkModelCmd, Integer> deadLinksNMOp = new Operation1<ChangeNetworkModelCmd, Integer>() {
+	static Operation1<ChangeNetworkModelCmd, Integer> deadLinksNMOp = new Operation1<ChangeNetworkModelCmd, Integer>() {
 
-        public ChangeNetworkModelCmd generate(Integer setIndex) {
-            NetworkModel baseNetworkModel = new UniformRandomModel(50, 500);
-            NetworkModel compositeNetworkModel = new DeadLinkNetworkModel(setIndex, baseNetworkModel, deadLinksSets.get(setIndex));
-            return new ChangeNetworkModelCmd(compositeNetworkModel);
-        }
-    };
+		public ChangeNetworkModelCmd generate(Integer setIndex) {
+			NetworkModel baseNetworkModel = new UniformRandomModel(50, 500);
+			NetworkModel compositeNetworkModel = new DeadLinkNetworkModel(
+					setIndex, baseNetworkModel, deadLinksSets.get(setIndex));
+			return new ChangeNetworkModelCmd(compositeNetworkModel);
+		}
+	};
 
-    static Operation<SimulationResult> simulationResult = new Operation<SimulationResult>() {
+	static Operation<SimulationResult> simulationResult = new Operation<SimulationResult>() {
 
-        public SimulationResult generate() {
-            return new SimulationResult() {
+		public SimulationResult generate() {
+			return new SimulationResult() {
 
-                public void setSimulationResult(OperationCmd.ValidationException failureCause) {
-                    SwimSimulationResult.failureCause = failureCause;
-                }
-            };
-        }
-    };
+				public void setSimulationResult(
+						OperationCmd.ValidationException failureCause) {
+					SwimSimulationResult.failureCause = failureCause;
+				}
+			};
+		}
+	};
 
-    //Operations require Distributions as parameters
-    //1.ConstantDistribution - this will provide same parameter no matter how many times it is called
-    //2.BasicIntSequentialDistribution - on each call it gives the next int. Works more or less like a counter
-    //3.GenIntSequentialDistribution - give it a vector. It will draw elements from it on each call. 
-    //Once out of elements it will give null. 
-    //So be carefull for null pointer exception if you draw more times than elements
-    //check se.sics.p2ptoolbox.simulator.dsl.distribution for more distributions
-    //you can implement your own - by extending Distribution
-    public static SimulationScenario simpleBoot(final long seed) {
-        SwimScenario.seed = seed;
-        SimulationScenario scen = new SimulationScenario() {
-            {
-                StochasticProcess startAggregator = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(1, startAggregatorOp, new ConstantDistribution(Integer.class, 0));
-                    }
-                };
+	// Operations require Distributions as parameters
+	// 1.ConstantDistribution - this will provide same parameter no matter how
+	// many times it is called
+	// 2.BasicIntSequentialDistribution - on each call it gives the next int.
+	// Works more or less like a counter
+	// 3.GenIntSequentialDistribution - give it a vector. It will draw elements
+	// from it on each call.
+	// Once out of elements it will give null.
+	// So be carefull for null pointer exception if you draw more times than
+	// elements
+	// check se.sics.p2ptoolbox.simulator.dsl.distribution for more
+	// distributions
+	// you can implement your own - by extending Distribution
+	public static SimulationScenario simpleBoot(final long seed) {
+		SwimScenario.seed = seed;
+		SimulationScenario scen = new SimulationScenario() {
+			{
+				StochasticProcess startAggregator = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(1, startAggregatorOp, new ConstantDistribution(
+								Integer.class, 0));
+					}
+				};
 
-                StochasticProcess startPeers = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(3, startNodeOp, new GenIntSequentialDistribution(new Integer[]{10, 14, 16}));
-                    }
-                };
+				StochasticProcess startPeers = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(3, startNodeOp, new GenIntSequentialDistribution(
+								new Integer[] { 10, 14, 16 }));
+					}
+				};
 
-                StochasticProcess killPeers = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(1, killNodeOp, new ConstantDistribution(Integer.class, 10));
-                    }
-                };
+				StochasticProcess killPeers = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(1, killNodeOp, new ConstantDistribution(
+								Integer.class, 10));
+					}
+				};
 
-                StochasticProcess deadLinks1 = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(1, deadLinksNMOp, new ConstantDistribution(Integer.class, 1));
-                    }
-                };
+				StochasticProcess deadLinks1 = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(1, deadLinksNMOp, new ConstantDistribution(
+								Integer.class, 1));
+					}
+				};
 
-                StochasticProcess disconnectedNodes1 = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(1, disconnectedNodesNMOp, new ConstantDistribution(Integer.class, 1));
-                    }
-                };
+				StochasticProcess disconnectedNodes1 = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(1, disconnectedNodesNMOp,
+								new ConstantDistribution(Integer.class, 1));
+					}
+				};
 
-                StochasticProcess fetchSimulationResult = new StochasticProcess() {
-                    {
-                        eventInterArrivalTime(constant(1000));
-                        raise(1, simulationResult);
-                    }
-                };
+				StochasticProcess fetchSimulationResult = new StochasticProcess() {
+					{
+						eventInterArrivalTime(constant(1000));
+						raise(1, simulationResult);
+					}
+				};
 
-                startAggregator.start();
-                startPeers.startAfterTerminationOf(1000, startAggregator);
-//                stopPeers.startAfterTerminationOf(10000, startPeers);
-//                deadLinks1.startAfterTerminationOf(10000,startPeers);
-//                disconnectedNodes1.startAfterTerminationOf(10000, startPeers);
-                fetchSimulationResult.startAfterTerminationOf(30*1000, startPeers);
-                terminateAfterTerminationOf(10000, fetchSimulationResult);
+				startAggregator.start();
+				startPeers.startAfterTerminationOf(1000, startAggregator);
+				killPeers.startAfterTerminationOf(10000, startPeers);
+				// deadLinks1.startAfterTerminationOf(10000,startPeers);
+				// disconnectedNodes1.startAfterTerminationOf(10000,
+				// startPeers);
+				fetchSimulationResult.startAfterTerminationOf(30 * 1000,
+						startPeers);
+				terminateAfterTerminationOf(10000, fetchSimulationResult);
 
-            }
-        };
+			}
+		};
 
-        scen.setSeed(seed);
+		scen.setSeed(seed);
 
-        return scen;
-    }
+		return scen;
+	}
 }
