48ead3d6de532ac1e45d8fdf8b3db6ae3b130147
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-05-21 17:29:02 +0200

    remove override

--- src/main/java/se/kth/swim/MembershipListInfectionSortPolicy.java
+++ src/main/java/se/kth/swim/MembershipListInfectionSortPolicy.java
@@ -6,7 +6,6 @@ public class MembershipListInfectionSortPolicy implements Comparator<MembershipL
 
 	public MembershipListInfectionSortPolicy() {}
 
-  @Override
 	public int compare(MembershipListItem o1, MembershipListItem o2) {
 		return o1.getInfectionTime() < o2.getInfectionTime() ? -1 :
 			o1.getInfectionTime() == o2.getInfectionTime() ? 0 : 1;
